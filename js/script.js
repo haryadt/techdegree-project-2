fbb1d1ea740dc617fbc81b0a5713d2604a1851e1
haryadt <145812740+haryadt@users.noreply.github.com>
2023-09-29 18:10:20 -0700

    Added pagination and search feature

--- js/script.js
+++ js/script.js
@@ -10,8 +10,22 @@ For assistance:
    Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
-const itemsPerPage = 20;
+const itemsPerPage = 9;
 
+function createElement(tag, classList, insertElement) {
+   const element = document.createElement(tag);
+
+   if(classList.length != 0) {
+      for(let classItem of classList) {
+         element.classList.add(classItem);
+      }
+   }
+
+   if(insertElement) {
+      insertElement.insertAdjacentElement("beforeend", element);
+   }
+   return element;
+}
 
 /*
 Create the `showPage` function
@@ -23,46 +37,31 @@ function showPage(list, page) {
 
    const studentList = document.querySelector(".student-list");
    studentList.innerHTML = "";
-   const studentListUl = document.querySelector(".student-list");
 
-   function createElementAndAddClasses(tag, classList) {
-      const element = document.createElement(tag);
-      element.classList.add(classList.join(", "));
-   }
 
-   for(let i = 0; i <= list.length; i++) {
+
+   for(let i = 0; i < list.length; i++) {
       if(i >= startIndex && i < endIndex) {
-         const listElement = document.createElement("li");
-         listElement.classList.add("student-item", "cf");
 
-         const studentDetailsElement = document.createElement("div");
-         studentDetailsElement.classList.add("student-details");
-         listElement.insertAdjacentElement("beforeend", studentDetailsElement);
+         const listElement = createElement("li", ["student-item", "cf"]);
 
-         const imageElement = document.createElement("img");
-         imageElement.classList.add("avatar");
+         const studentDetailsElement = createElement("div", ["student-details"], listElement);
+
+         const imageElement = createElement("img", ["avatar"], studentDetailsElement);
          imageElement.src = list[i].picture.large;
-         studentDetailsElement.insertAdjacentElement("beforeend", imageElement);
 
-         const headerThree = document.createElement("h3");
+         const headerThree = createElement("h3", [], studentDetailsElement);
          headerThree.innerText = `${list[i].name.first} ${list[i].name.last}`;
-         studentDetailsElement.insertAdjacentElement("beforeend", headerThree);
 
-         const emailSpan = document.createElement("span");
-         emailSpan.classList.add("email");
+         const emailSpan = createElement("span", ["email"], studentDetailsElement);
          emailSpan.innerText = `${list[i].email}`;
-         studentDetailsElement.insertAdjacentElement("beforeend", emailSpan);
 
-         const joinedDetails = document.createElement("div");
-         joinedDetails.classList.add("joined-details");
-         listElement.insertAdjacentElement("beforeend", joinedDetails);
+         const joinedDetails = createElement("div", ["joined-details"], listElement);
 
-         const dateSpan = document.createElement("span");
-         dateSpan.classList.name = "date"
+         const dateSpan = createElement("span", ["date"], joinedDetails);
          dateSpan.innerText= `${list[i].registered.date}`;
-         joinedDetails.insertAdjacentElement("beforeend", dateSpan);
 
-         studentListUl.append(listElement);
+         studentList.append(listElement);
       }
    }
 }
@@ -72,6 +71,78 @@ function showPage(list, page) {
 Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
+function addPagination(list) {
+   const numberOfPaginationButtons = Math.floor(list.length / itemsPerPage);
+   const linkList = document.querySelector("ul.link-list");
+   linkList.innerHTML = "";
+   
+   for(let i = 1; i <= numberOfPaginationButtons; i++) {
+      let buttonList = document.createElement("li");
+      let button = document.createElement("button");
+      button.innerText = i;
+      button.type = "button";
+      buttonList.append(button);
+      linkList.insertAdjacentElement("beforeend", buttonList);
+   }
+
+   if(linkList.innerHTML !== "") {
+      linkList.firstChild.firstChild.classList.add("active");
+   }
+
+   linkList.addEventListener("click", (e) => {
+      if(e.target.tagName === "BUTTON") {
+         for(let list of linkList.childNodes) {
+            list.firstChild.classList.remove("active");
+         }
+         e.target.classList.add("active");
+         showPage(list, e.target.innerText);
+      }
+   });
+}
+
+function createStudentSearch(datta) {
+   const label = createElement("label", ["student-search"]);
+   label.setAttribute("for", "search");
+
+   const input = createElement("input", [], label);
+   input.id = "search";
+   input.placeholder = "Search by name...";
+
+   const button = createElement("button", [], label);
+   button.type = "button";
+
+   const img = createElement("img", [], button);
+   img.src = "img/icn-search.svg";
+   img.alt = "Search icon";
+
+   const header = document.getElementsByClassName("header")[0];
+   header.insertAdjacentElement("beforeend", label);
+
+   searchStudent(data, input, button);
+}
+
+function searchStudent(data, searchElement, searchButtonElement) {
+   if(searchElement) {
+      const performSearch = (target) => {
+         const newData = [];
+         const userInput = target.value.toLowerCase();
+         for(let i =0; i < data.length; i++) {
+            const studentName = data[i].name.first.toLowerCase() + " " + data[i].name.last.toLowerCase();
+            if(studentName.includes(userInput)){
+               newData.push(data[i]);
+            }
+         }
+         showPage(newData, 1);
+         addPagination(newData);
+
+      }; 
+      searchElement.addEventListener("keyup", (e) => {performSearch(e.target)});
+      searchElement.addEventListener("submit", (e) => {performSearch(e.target)});
+      searchButtonElement.addEventListener("click", (e) => {performSearch(searchButtonElement.previousSibling)});
+   }
+}
 
 // Call functions
+createStudentSearch();
 showPage(data, 1);
+addPagination(data);
